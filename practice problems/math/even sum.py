0104bcc57eb5789f96a9b447bcb0d9e1d45c679d
vkrmjeet <67248881+vkrmjeet@users.noreply.github.com>
2022-03-20 07:04:05 +0530

    Delete practice problems/math directory

--- practice problems/math/even sum.py
+++
@@ -1,8 +0,0 @@
-for _ in range(int(input())):
-    n = int(input())
-    lis = list(map(int,input().split()))
-    res = sum(lis)
-    if res%2==0:
-        print(1)
-    else:
-        print(2)
--- practice problems/math/last digit.py
+++
@@ -1,3 +0,0 @@
-n = int(input())
-d = int(n%10)
-print(d)
