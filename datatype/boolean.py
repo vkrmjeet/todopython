4a2d13f76f20045912d43daef10db130fd5b3522
vkrmjeet <67248881+vkrmjeet@users.noreply.github.com>
2022-03-20 07:01:11 +0530

    Delete datatype directory

--- datatype/boolean.py
+++
@@ -1,7 +0,0 @@
-'''
-boolean type have two values just true and false
-'''
-
-type(True) # this will return bool data type in python console
-
-type(False)   #this will return bool data type in python console
