256d59d45d0f070a0ade3c64a5cc592a8bbc3961
vkrmjeet <67248881+vkrmjeet@users.noreply.github.com>
2022-03-20 07:00:47 +0530

    Delete bootstrap directory

--- bootstrap/installing bootstrap.py
+++
@@ -1,7 +0,0 @@
-'''
-      download from website
-      getting it from CDN(content delivery network) (it automatically updates bootstrap in our machine)
-             https://getbootstrap.com/docs/5.1/getting-started/introduction/
-
-
-'''
--- bootstrap/overview.py
+++
@@ -1,9 +0,0 @@
-'''
-    bootstrap framework overview :
-              it contains css,components,javascript and other customizable scripts.
-    it has  a grid system used to create rows and columns.
-    you need to wrap your code into an container.
-    many types of components can be used to design your website better.
-
-    you can also customize and include and remove some of the functionality of bootstrap from getbootstrap.com
-'''
