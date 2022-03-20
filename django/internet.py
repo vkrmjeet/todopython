2b172e8150616871e14347521a01fd113c80d5bf
vkrmjeet <67248881+vkrmjeet@users.noreply.github.com>
2022-03-20 07:01:37 +0530

    Delete django directory

--- django/internet.py
+++
@@ -1,7 +0,0 @@
-'''
-      world wide web : the web is a collection of webpages found on the internet.
-      the internet is a huge network of connected computers.
-      web browser is used to access the internet .
-      the client server network is used
-
-'''
